928e7a8bd4846ca1b78c16397fa8eec1875ea835
Yongli <yl.low91@gmail.com>
2023-04-21 15:21:07 +0800

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'day-35-funapp';
+export class AppComponent implements OnInit {
+  
+  options!: AnimationOptions
+
+  ngOnInit(): void {
+      this.options = {
+        path: '/assets/moody-giraffe.json',
+      }
+  }
 }
